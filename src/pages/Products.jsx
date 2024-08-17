10b6abaf7a1680e8e544a46f6de9f9721dc39f91
RakibHassanSoft <rakibreings32@gmail.com>
2024-08-17 15:49:07 +0600

    product file is missing

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ProductCard from './ProductCard';
+
 
 const Products = () => {
   const [products, setProducts] = useState([]);
